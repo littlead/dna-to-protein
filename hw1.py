5332efe4496c66a9ddf8083cfaf2bc64d61912fd
Squee <squee@pop-os.localdomain>
2019-10-04 10:24:26 -0700

    This is technically working, but the approach is wrong. Need every OPEN reading frame , but this is overkill taking every READING FRAME into account.

--- hw1.py
+++ hw1.py
@@ -1,5 +1,17 @@
+import re
 
-data = 'ATAAGC'
+file = open('pa1.fasta', 'r')
+data = file.read()
+split_lines = data.splitlines()
+sequence = ""
+for line in split_lines:
+    if line.startswith('>'):
+        pass
+    else:
+        sequence = sequence + line
+
+file.close()
+
 
 amino = {
     'GCT': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
@@ -22,8 +34,8 @@ amino = {
     'ATG': 'M',
     'AAT': 'N', 'AAC': 'N',
     'CAA': 'Q', 'CAG': 'Q',
-    'ATG': 'START',
-    'TAA': 'STOP', 'TGA': 'STOP', 'TAG': 'STOP',
+    'ATG': 'Start',
+    'TAA': 'Stop', 'TGA': 'Stop', 'TAG': 'Stop',
 }
 
 # General function used to tranlate each ORF
@@ -33,20 +45,24 @@ def protein(sequence, orf, start, stop):
         orf.append(amino[codon])
 
 def dna_to_protein(sequence):
-    
+
     # One amino acid sequence for each ORF
     orf1 = []
     orf2 = []
     orf3 = []
 
-    protein(sequence, orf1, 0, len(sequence))
-    protein(sequence, orf2, 1, len(sequence)-2)
-    protein(sequence, orf3, 2, len(sequence)-1)
+    protein(sequence, orf1, 0, len(sequence)-2)
+    protein(sequence, orf2, 1, len(sequence)-1)
+    protein(sequence, orf3, 2, len(sequence))
 
     # Print to console for testing
-    print(orf1, orf2, orf3)
+    protein1 = ''.join(orf1)
+    protein2 = ''.join(orf2)
+    protein3 = ''.join(orf3)
 
-    return orf1, orf2, orf3
+    print(protein1, protein2, protein3)
+
+    return protein1, protein2, protein3
 
 # Call Function
-dna_to_protein(data)
+dna_to_protein(sequence)
